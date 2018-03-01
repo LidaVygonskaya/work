1ae9d8c5f653c7eb412da54776fec86cf8effabd
unknown <Vygonskaya.lyu@phystech.edu>
2018-03-01 17:27:03 +0300

    newtons method

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
+import math
 from matplotlib import pyplot as plt
+import copy
 #initial data
 #prostranstvo
 x_0 = 0
@@ -36,6 +38,10 @@ x = np.arange(x_0, x_N, (x_N - x_0) / N)
 x = np.append(x, x_N)
 x = list(x)
 
+#Newtons method
+delta_0 = 1000.0 #nachalnoe priblizhenie
+delta_max = 10 ** (-3) #zadannaya tochnost
+delta_gr = 1000.0
 
 #coefficients
 
@@ -57,7 +63,7 @@ def count_d(P_i_n):
     return -(P_i_n * h * fi * c_f / tau)
 
 
-def thomas_method(list_a, list_b, list_c, list_d):
+def thomas_method(list_a, list_b, list_c, list_d, P0, PN):
     w_1 = list_a[0]
     q_list = []
     w_list = [w_1]
@@ -77,9 +83,9 @@ def thomas_method(list_a, list_b, list_c, list_d):
         P = g_list[j] - q_list[j] * list_P[-1]
         list_P.append(P)
     # Add usloviya na granitsah
-    list_P.append(P_0)
+    list_P.append(P0)
     list_P.reverse()
-    list_P.append(P_Nx)
+    list_P.append(PN)
     return list_P
 
 
@@ -141,8 +147,8 @@ def initialize_d_nonlinear(list_P, list_c, list_b):
         d = count_d_nonlinear(list_P[j])
         list_d.append(d)
 
-    list_d[0] -= list_c[0] * list_P[0]
-    list_d[-1] -= list_b[-1] * list_P[-1]
+    #list_d[0] -= list_c[0] * list_P[0]
+    #list_d[-1] -= list_b[-1] * list_P[-1]
     return list_d
 
 
@@ -161,46 +167,159 @@ def initialize_abc(list_P, x_list):
     return list_abc
 
 
-P_list = initialize_P(P_0, P_Nx, P_init)
-a_list = []
-b_list = []
-c_list = []
+#Newtons method
+def count_discrepancy(list_abcd, list_p, index):
+    a, b, c, d = list_abcd
+    return c[index] * list_p[index] + a[index] * list_p[index + 1]  + b[index] * list_p[index + 2] - d[index]
 
-for i in range(1, N):
-    a = count_a_nonlinear(P_list, i, x)
-    b = count_b_nonlinear(P_list, i, x)
-    c = count_c_nonlinear(P_list, i, x)
-    a_list.append(a)
-    b_list.append(b)
-    c_list.append(c)
 
-d_list = initialize_d_nonlinear(P_list, c_list, b_list)
+def diff_simple(list_a, list_b, list_c):
+    list_diff = []
+    for j in range(N - 1):
+        list_diff_i = [list_c[j], list_a[j], list_b[j]]
+        list_diff.append(list_diff_i)
+    return list_diff
 
+
+def diff(list_p_k, list_delta, list_abcd):
+    list_diff = []
+    for j in range(N - 1):
+        R_minus = list_abcd[2][j] * (list_p_k[j] + list_delta[j]) + list_abcd[0][j] * list_p_k[j + 1] + list_abcd[1][j] * list_p_k[j + 2]
+
+        R_0 = list_abcd[2][j] * list_p_k[j] + list_abcd[0][j] * (list_p_k[j + 1] + list_delta[j + 1]) + list_abcd[1][j] * list_p_k[j + 2]
+
+        R_plus = list_abcd[2][j] * list_p_k[j] + list_abcd[0][j] * list_p_k[j + 1] + list_abcd[1][j] * (list_p_k[j + 2] + list_delta[j + 2])
+
+        R_diff_minus = (R_minus - count_discrepancy(list_abcd, list_p_k, j)) / list_delta[j]
+        R_diff_0 = (R_0 - count_discrepancy(list_abcd, list_p_k, j)) / list_delta[j + 1]
+        R_diff_plus = (R_plus - count_discrepancy(list_abcd, list_p_k, j)) / list_delta[j + 2]
+        list_diff_j = [R_diff_minus, R_diff_0, R_diff_plus]
+        list_diff.append(list_diff_j)
+    return list_diff
+
+
+def check_diag(a, b, c, index):
+    if math.fabs(a) <= math.fabs(b) + math.fabs(c):
+        print("ne shoditsa " + str(index))
+
+
+def count_norm(list_delta):
+    list_delta_abs = [math.fabs(elem) for elem in list_delta[1:-1]]
+    return max(list_delta_abs)
+
+
+#NEWTONS METHOD
+P_list_k = initialize_P(P_0, P_Nx, P_init) #init
+P_list_n = initialize_P(P_0, P_Nx, P_init)
 time = tau
 counter = 1
 
-while time <= time_max:
-    P_list = thomas_method(a_list, b_list, c_list, d_list)
+while time < time_max:
+    delta_list_k = [delta_gr for i in range(len(x))]
+    for i in range(1, len(delta_list_k)):
+        delta_list_k[i] = delta_0
+
+    while count_norm(delta_list_k) > delta_max:
+        print("norm " + str(count_norm(delta_list_k)))
+        a_list = []
+        b_list = []
+        c_list = []
+        for i in range(1, N):
+            a = count_a_nonlinear(P_list_k, i, x)
+            b = count_b_nonlinear(P_list_k, i, x)
+            c = count_c_nonlinear(P_list_k, i, x)
+            check_diag(a, b, c, i)
+
+            a_list.append(a)
+            b_list.append(b)
+            c_list.append(c)
+
+        d_list = initialize_d_nonlinear(P_list_n, c_list, b_list)
+        abcd_list = [a_list, b_list, c_list, d_list]
+
+        R_list = []
+        for i in range(len(a_list)):
+            R = count_discrepancy(abcd_list, P_list_k, i)
+            R_list.append(R)
+
+        #R_diff = diff(P_list_k, delta_list_k, abcd_list)
+        R_diff = diff_simple(a_list, b_list, c_list)
+        a_delta_list = []
+        b_delta_list = []
+        c_delta_list = []
+
+        for i in range(N - 1):
+            a_delta = R_diff[i][1]
+            b_delta = R_diff[i][2]
+            c_delta = R_diff[i][0]
+            check_diag(a_delta, b_delta, c_delta, i)
+
+            a_delta_list.append(a_delta)
+            b_delta_list.append(b_delta)
+            c_delta_list.append(c_delta)
+
+        c_delta_list[0] = 0.0
+        b_delta_list[-1] = 0.0
+
+        d_delta_list = [- elem for elem in R_list]
+
+        delta_list_k = thomas_method(a_delta_list, b_delta_list, c_delta_list, d_delta_list, delta_gr, delta_gr)
+        for i in range(1, len(P_list_k) - 1):
+            P_list_k[i] = P_list_k[i] + delta_list_k[i]
+
+    P_list_n = copy.deepcopy(P_list_k)
     if counter % 10 == 0:
-        name = 'graph_nonlinear ' + str(counter)
-        plt.plot(x, P_list)
+        name = 'graph_' + str(counter)
+        plt.plot(x, P_list_n)
         plt.xlabel('x')
-        plt.ylabel('P')
+        plt.ylabel('y')
         plt.savefig(name)
         plt.clf()
     counter += 1
     time += tau
-    a_list = []
-    b_list = []
-    c_list = []
-    for i in range(1, N):
-        a = count_a_nonlinear(P_list, i, x)
-        b = count_b_nonlinear(P_list, i, x)
-        c = count_c_nonlinear(P_list, i, x)
-        a_list.append(a)
-        b_list.append(b)
-        c_list.append(c)
-    d_list = initialize_d_nonlinear(P_list, c_list, b_list)
+
+
+#NONLINEAR PART
+# P_list = initialize_P(P_0, P_Nx, P_init)
+# a_list = []
+# b_list = []
+# c_list = []
+#
+# for i in range(1, N):
+#     a = count_a_nonlinear(P_list, i, x)
+#     b = count_b_nonlinear(P_list, i, x)
+#     c = count_c_nonlinear(P_list, i, x)
+#     a_list.append(a)
+#     b_list.append(b)
+#     c_list.append(c)
+#
+# d_list = initialize_d_nonlinear(P_list, c_list, b_list)
+#
+# time = tau
+# counter = 1
+#
+# while time <= time_max:
+#     P_list = thomas_method(a_list, b_list, c_list, d_list)
+#     if counter % 10 == 0:
+#         name = 'graph_nonlinear ' + str(counter)
+#         plt.plot(x, P_list)
+#         plt.xlabel('x')
+#         plt.ylabel('P')
+#         plt.savefig(name)
+#         plt.clf()
+#     counter += 1
+#     time += tau
+#     a_list = []
+#     b_list = []
+#     c_list = []
+#     for i in range(1, N):
+#         a = count_a_nonlinear(P_list, i, x)
+#         b = count_b_nonlinear(P_list, i, x)
+#         c = count_c_nonlinear(P_list, i, x)
+#         a_list.append(a)
+#         b_list.append(b)
+#         c_list.append(c)
+#     d_list = initialize_d_nonlinear(P_list, c_list, b_list)
 
 
 
