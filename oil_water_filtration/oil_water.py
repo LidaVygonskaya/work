e750df27fe527ef900fea139d4ed88d0d63bef56
lida <Vygonskaya.lyu@phystech.edu>
2018-04-02 23:23:17 +0300

    added s counting

--- oil_water_filtration/oil_water.py
+++ oil_water_filtration/oil_water.py
@@ -67,6 +67,23 @@ class Layer:
     def count_fi(self, pressure_oil):
         return self.fi_0 * (1.0 + self.c_r * (pressure_oil - self.P_01))
 
+    @staticmethod
+    def count_pcap(p_cap_graph, s_water_list):
+        p_cap_list = []
+        s_w_graph = list(p_cap_graph.keys())
+        for i in range(len(s_water_list)):
+            for j in range(len(s_w_graph)):
+                if s_water_list[i] < s_w_graph[j]:
+                    p_cap = p_cap_graph.get(s_w_graph[j - 1]) + (
+                                p_cap_graph.get(s_w_graph[j]) - p_cap_graph.get(
+                            s_w_graph[j - 1])) / (s_w_graph[j] - s_w_graph[j - 1]) * (
+                                        s_water_list[i] - s_w_graph[j - 1])
+                    p_cap_list.append(p_cap)
+                    break
+        return p_cap_list
+
+
+
 
 class Solver:
     def __init__(self):
@@ -105,12 +122,12 @@ class Solver:
         return t_x_oil_plus
 
     @staticmethod
-    def count_c1_p(layer, fi, s_water, ro_water, index ):
-        return (fi[index] * s_water[index] * layer.ro_water_0 * layer.c_f_water) + (s_water[index] * ro_water[index] * layer.c_r * layer.fi_0)
+    def count_c1_p(layer, fi_n, s_water, ro_water, index ):
+        return (fi_n[index] * s_water[index] * layer.ro_water_0 * layer.c_f_water) + (s_water[index] * ro_water[index] * layer.c_r * layer.fi_0)
 
     @staticmethod
-    def count_c2_p(layer, fi, s_water, ro_oil, index):
-        return (1.0 - s_water[index]) * (fi[index] * layer.ro_oil_0 * layer.c_f_oil + layer.c_r * layer.fi_0 * ro_oil[index])
+    def count_c2_p(layer, fi_n, s_water, ro_oil, index):
+        return (1.0 - s_water[index]) * (fi_n[index] * layer.ro_oil_0 * layer.c_f_oil + layer.c_r * layer.fi_0 * ro_oil[index])
 
 
     @staticmethod
@@ -184,6 +201,11 @@ s_water = []
 for i in range(layer.N - 2):
     s_water.append(layer.s_water_init)
 
+#initialize S_oil
+s_oil = []
+for i in range(layer.N - 2):
+    s_oil.append(1 - layer.s_water_init)
+
 #initialize Oil pressure without gr
 P_oil = []
 for j in range(layer.N - 2):
@@ -209,6 +231,10 @@ for i in range (len(P_oil)):
 s_water = [layer.s_water_left] + s_water
 s_water.append(layer.s_water_right)
 
+#s_oil
+s_oil = [1 - layer.s_water_left] + s_oil
+s_oil.append(1 - layer.s_water_right)
+
 #P_cap
 P_cap = [pressure_cap_graph.get(s_water[0])] + P_cap
 P_cap.append(pressure_cap_graph.get(s_water[-1]))
@@ -217,53 +243,93 @@ print(P_cap)
 #P_water
 P_water = [layer.pressure_water_left] + P_water
 P_water.append(P_water[-1])
-print (P_water)
+
 
 #P_oil
 P_oil = [P_water[0] + P_cap[0]] + P_oil
 P_oil.append(P_oil[-1])
 
-delta_list_k = [solver.delta_0 for i in range(layer.N)]
-P_oil_n = P_oil.copy()
-while solver.count_norm(delta_list_k) > solver.delta_max:
-    print(solver.count_norm(delta_list_k))
-    a_list = []
-    b_list = []
-    c_list = []
-    d_list = []
-    t_water_minus_list = []
-    t_water_plus_list = []
-    t_oil_minus_list = []
-    t_oil_plus_list = []
-    ro_water = []
-    ro_oil = []
-    fi_list = []
+#fi_list_n
 
-    for i in range(1, layer.N - 1):
-        t_water_minus_list.append(solver.count_t_water_minus(layer, P_water, s_water, i))
-        t_oil_minus_list.append(solver.count_t_oil_minus(layer, P_oil, s_water, i))
-        t_water_plus_list.append(solver.count_t_water_plus(layer, P_water, s_water, i))
-        t_oil_plus_list.append(solver.count_t_oil_plus(layer, P_oil, s_water, i))
-        ro_water.append(layer.count_ro_water(P_water[i]))
-        ro_oil.append(layer.count_ro_oil(P_oil[i]))
-        fi_list.append(layer.count_fi(P_oil[i]))
+time = solver.tau
+counter = 1
+s_water_from_oil = []
+while time < solver.time_max:
+
+    delta_list_k = [solver.delta_0 for i in range(layer.N)]
+    P_oil_n = P_oil.copy()
+    s_water_n = s_water.copy()
+    s_oil_n = s_oil.copy()
+    fi_list_n = [layer.count_fi(P_oil_n[i]) for i in range(1, layer.N - 1)]
+    P_cap = [pressure_cap_graph.get(s_water_n[0])] + layer.count_pcap(pressure_cap_graph, s_water_n[1:-1])
+    P_cap.append(pressure_cap_graph.get(s_water_n[-1]))
+    print(len(P_cap))
+
+
+    while solver.count_norm(delta_list_k) > solver.delta_max:
+
+        #print(solver.count_norm(delta_list_k))
+        a_list = []
+        b_list = []
+        c_list = []
+        d_list = []
+        t_water_minus_list = []
+        t_water_plus_list = []
+        t_oil_minus_list = []
+        t_oil_plus_list = []
+        ro_water = []
+        ro_oil = []
+        fi_list = []
+
+        for i in range(1, layer.N - 1):
+            t_water_minus_list.append(solver.count_t_water_minus(layer, P_water, s_water, i))
+            t_oil_minus_list.append(solver.count_t_oil_minus(layer, P_oil, s_water, i))
+            t_water_plus_list.append(solver.count_t_water_plus(layer, P_water, s_water, i))
+            t_oil_plus_list.append(solver.count_t_oil_plus(layer, P_oil, s_water, i))
+            ro_water.append(layer.count_ro_water(P_water[i]))
+            ro_oil.append(layer.count_ro_oil(P_oil[i]))
+            fi_list.append(layer.count_fi(P_oil[i]))
+
+        for i in range(layer.N - 2):
+            b_list.append(solver.count_b(ro_water, ro_oil, t_water_plus_list[i], t_oil_plus_list[i], i))
+            c_list.append(solver.count_c(ro_water, ro_oil, t_water_minus_list[i], t_oil_minus_list[i], i))
+            c1_p = solver.count_c1_p(layer, fi_list_n, s_water[1:-1], ro_water, i)
+            c2_p = solver.count_c2_p(layer, fi_list_n, s_water[1:-1], ro_oil, i)
+            a_list.append(solver.count_a(ro_water, ro_oil, c1_p, c2_p, b_list[i], c_list[i], i))
+            d_list.append(solver.count_d(P_oil,P_oil_n, P_cap, ro_water, ro_oil, t_water_minus_list[i], t_water_plus_list[i], a_list, b_list, c_list, c1_p, c2_p, i))
+
+        delta_list_k = solver.thomas_method(a_list, b_list, c_list, d_list, solver.delta_0, solver.delta_0)
+        for i in range(1, layer.N - 1):
+            P_oil[i] = P_oil[i] + delta_list_k[i]
 
     for i in range(layer.N - 2):
-        b_list.append(solver.count_b(ro_water, ro_oil, t_water_plus_list[i], t_oil_plus_list[i], i))
-        c_list.append(solver.count_c(ro_water, ro_oil, t_water_minus_list[i], t_oil_minus_list[i], i))
-        c1_p = solver.count_c1_p(layer, fi_list, s_water[1:-1], ro_water, i)
-        c2_p = solver.count_c2_p(layer, fi_list, s_water[1:-1], ro_oil, i)
-        a_list.append(solver.count_a(ro_water, ro_oil, c1_p, c2_p, b_list[i], c_list[i], i))
-        d_list.append(solver.count_d(P_oil,P_oil_n, P_cap, ro_water, ro_oil, t_water_minus_list[i], t_water_plus_list[i], a_list, b_list, c_list, c1_p, c2_p, i))
+        s_water[i + 1] = s_water_n[i + 1] + (solver.tau/(fi_list[i] * ro_water[i])) * (t_water_plus_list[i] * (P_oil[i + 2] - P_oil_n[i + 1]) + t_water_minus_list[i] * (P_oil[i] - P_oil[i + 1]))
+        s_oil[i + 1] = s_oil_n[i + 1] + ((solver.tau/(fi_list[i] * ro_oil[i]))) * (t_oil_plus_list[i] * (P_oil[i + 2] - P_oil[i + 1] + P_cap[i + 2] - P_cap[i + 1]) + t_oil_minus_list[i] * (P_oil[i] - P_oil[i + 1] + P_cap[i] - P_cap[i + 1]))
 
-    delta_list_k = solver.thomas_method(a_list, b_list, c_list, d_list, solver.delta_0, solver.delta_0)
-    for i in range(1, layer.N - 1):
-        P_oil[i] = P_oil[i] + delta_list_k[i]
+    s_water_from_oil = [1 - s_oil[i] for i in range(len(s_oil))]
+    print(solver.count_norm([s_water[i] - s_water_n[i] for i in range(len(s_water))]))
 
-x = np.arange(layer.x_0, layer.x_N, layer.h)
-x = np.append(x, layer.N)
-plt.plot(x, P_oil)
-plt.show()
+    if solver.count_norm([s_water[i] - s_water_n[i] for i in range(len(s_water))]) > 0.1:
+        solver.tau = solver.tau / 2.0
+        P_oil = P_oil_n.copy()
+        s_water = s_water_n.copy()
+        fi_list = fi_list_n.copy()
+
+    else:
+        x = np.arange(layer.x_0, layer.x_N, layer.h)
+        x = np.append(x, layer.N)
+        k_r_water = [s_water[i] ** 2 for i in range(len(s_water))]
+        k_r_oil = [(1 - s_water[i]) ** 2 for i in range(len(s_water))]
+        plt.plot(x, P_oil)
+        plt.show()
+        plt.plot(s_water, k_r_water)
+        plt.plot(s_water, k_r_oil)
+        plt.show()
+
+        time += solver.tau
+        counter += 1
+    #Count S n + 1
+
 
 
 
